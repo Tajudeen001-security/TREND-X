abaee761eb8621fc6969a959cc057f8cf7276ce0
Tajudeen001-security <gbadamositajudeenwan@gmail.com>
2025-08-11 22:24:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "trend-x~T0oFVbRJ#GSguhoFig9fdwZuQB5QDLIJYrCzrrVq7oP3VVbtFMa8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
